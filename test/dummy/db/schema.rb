0ead78a81de85cc9c00ad02ed4cf50a72dd428aa
TheDumbTechGuy <sfroelich01@gmail.com>
2026-07-04 21:47:06 +0000

    Fix CI: stamp dummy schema at [7.1] so all supported Rails versions load it

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_04_000101) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_04_000101) do
   create_table "angarium_deliveries", force: :cascade do |t|
     t.integer "attempt_count", default: 0, null: false
     t.datetime "created_at", null: false
